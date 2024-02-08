45915826e46a2f48c5f61722b9056d4aa4633b19
SerhiiAnd <136363351+SerhiiAnd@users.noreply.github.com>
2024-02-08 10:24:13 +0100

    save08.02

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,59 +1,117 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 import css from 'components/ContactForm/ContactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+const ContactForm = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  handleChange = e => {
+  const handleChange = e => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'number') {
+      setNumber(value);
+    }
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    const { name, number } = this.state;
     const contact = { id: nanoid(), name, number };
-    this.props.onAddContact(contact);
+    onAddContact(contact);
 
-    this.setState({ name: '', number: '' });
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <form className={css.form} onSubmit={this.handleSubmit}>
-        <label className={css.text}>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-            required
-          />
-        </label>
-        <label className={css.text}>
-          Number:
-          <input
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-            required
-          />
-        </label>
-        <button className={css.buttonStyle} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <label className={css.text}>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label className={css.text}>
+        Number:
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <button className={css.buttonStyle} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 export default ContactForm;
+
+// import React, { Component } from 'react';
+// import { nanoid } from 'nanoid';
+// import css from 'components/ContactForm/ContactForm.module.css';
+
+// class ContactForm extends Component {
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   handleChange = e => {
+//     const { name, value } = e.target;
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     const { name, number } = this.state;
+//     const contact = { id: nanoid(), name, number };
+//     this.props.onAddContact(contact);
+
+//     this.setState({ name: '', number: '' });
+//   };
+
+//   render() {
+//     const { name, number } = this.state;
+
+//     return (
+//       <form className={css.form} onSubmit={this.handleSubmit}>
+//         <label className={css.text}>
+//           Name:
+//           <input
+//             type="text"
+//             name="name"
+//             value={name}
+//             onChange={this.handleChange}
+//             required
+//           />
+//         </label>
+//         <label className={css.text}>
+//           Number:
+//           <input
+//             type="tel"
+//             name="number"
+//             value={number}
+//             onChange={this.handleChange}
+//             required
+//           />
+//         </label>
+//         <button className={css.buttonStyle} type="submit">
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+
+// export default ContactForm;
